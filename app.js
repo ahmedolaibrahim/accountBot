b4cb43ca565daff44ed64dabbb95fc41ae1a8188
Ibrahim Ahmed <ahmedolaibrahim@gmail.com>
2018-04-04 19:26:53 +0100

    willilaim hill signup page

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const app = express();
+const PORT = 9000;
+
+
+//Handlebars Middleware
+
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
+  
+app.set('view engine','handlebars');
+
+app.listen(PORT,()=>{
+    console.log("account bot POC started");
+});
+
+app.get('/',(req,res)=>{
+  res.render("index");
+});
+
+app.get('/signup',(req,res)=>{
+    res.render("signup");
+});
